71856ea36b6cc8c7b7a9aa72e7559ca484539f04
Alex P <143479828+MariaAlexandraPeralejo@users.noreply.github.com>
2025-11-01 17:00:15 -0700

    new file:   onpoint-cc-rewards/.env.example 	new file:   onpoint-cc-rewards/.gitignore 	modified:   onpoint-cc-rewards/backend-api/server.js 	modified:   onpoint-cc-rewards/database/connect.js 	new file:   onpoint-cc-rewards/docs/requirements.txt 	new file:   onpoint-cc-rewards/package-lock.json 	new file:   onpoint-cc-rewards/package.json

--- onpoint-cc-rewards/backend-api/server.js
+++ onpoint-cc-rewards/backend-api/server.js
@@ -1,1 +1,26 @@
-// Main express server
+/* Main express server*/
+
+const express = require('express');
+const connectDB = require('./database/connect');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Import routes
+const cardRoutes = require('./routes/cardRoutes');
+const userRoutes = require('./routes/authRoutes');
+const recommendationRoutes = require('./routes/recommendationRoutes');
+
+// Connect DB
+connectDB();
+
+// Use routes
+app.use('/api/cards', cardRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/recommendations', recommendationRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
