55cf79b0128c36716020ef0d07738d44f3349eb1
artur-a1 <aleksanianartur@gmail.com>
2025-12-07 18:25:53 -0800

    fix merge conflict in cardRoutes

--- onpoint-cc-rewards/backend-api/routes/cardRoutes.js
+++ onpoint-cc-rewards/backend-api/routes/cardRoutes.js
@@ -2,12 +2,9 @@
 
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
 const Card = require('../models/Card');
-=======
 const cardController = require('../controllers/cardController');
 const auth = require('../middleware/auth');
->>>>>>> cd94c93 (refactor: move authentication middleware to separate file and protect routes)
 
 // GET all cards
 router.get('/', async (req, res) => {
@@ -19,18 +16,6 @@ router.get('/', async (req, res) => {
   }
 });
 
-<<<<<<< HEAD
-// POST add new card
-router.post('/', async (req, res) => {
-  try {
-    const card = new Card(req.body);
-    const newCard = await card.save();
-    res.status(201).json(newCard);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-=======
 // POST add new card (protected)
 router.post('/', auth.authenticate, cardController.createCard);
 
@@ -38,6 +23,5 @@ router.post('/', auth.authenticate, cardController.createCard);
 router.get('/:id', cardController.loadCard, cardController.getCardById);
 router.patch('/:id', auth.authenticate, cardController.loadCard, cardController.updateCard);
 router.delete('/:id', auth.authenticate, cardController.loadCard, cardController.deleteCard);
->>>>>>> cd94c93 (refactor: move authentication middleware to separate file and protect routes)
 
 module.exports = router;
