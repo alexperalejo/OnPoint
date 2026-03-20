ba86330773f10e4f228e0bda2499968f6319b4d2
ChGharibian <chris@thevgroup.com>
2026-03-19 23:59:57 -0700

    available locales can be added to translation.js and the best locale should be chosen. Also renamed some parameters and corrected JSDoc comments.

--- onpoint-cc-rewards/frontend/src/utils/translation.js
+++ onpoint-cc-rewards/frontend/src/utils/translation.js
@@ -2,17 +2,41 @@
 
 import { useCallback } from 'react';
 
-var translation = {};
+var currentLocale = {};
+const availableLocales = [
+    "en-US"
+];
 
 document.addEventListener('DOMContentLoaded', async () => {  
     // Load and display shipped names  
     try {  
         // Fetch the shipped JSON file (relative path from popup.html)  
-        var language = chrome.i18n.getUILanguage();
-        console.log(language);
-        const response = await fetch("./languages/"+ language + ".json");  
+        var locale = chrome.i18n.getUILanguage();
+        
+        const splits = locale.split('-');
+        var matchedLocale = "en-US";
+        var matchStrength = 0;
+
+        for(const availableLocale in availableLocales){
+            const targetLocale = availableLocale.split('-');
+            const matchableSegmentCount = Math.min(splits.length, targetLocale.length);
+            let i = 0;
+            for(; i < matchableSegmentCount; i++)
+            {
+                if(targetLocale[i] !== splits[i])
+                {
+                    break;
+                }
+            }
+            if(matchStrength < i) {
+                matchStrength = i;
+                matchedLocale = availableLocale;
+            }
+        }
+
+        const response = await fetch("./languages/" + locale + ".json");  
         if (!response.ok) throw new Error('Failed to load shipped names');  
-        translation = await response.json();  
+        currentLocale = await response.json();  
     } catch (error) {  
         console.error('Error loading translation:', error);  
     }  
@@ -20,12 +44,12 @@ document.addEventListener('DOMContentLoaded', async () => {
 
 /**
  * Returns a translator function.
- * @param {object} translation the locale
- * @param {string} key the key
- * @param {object?} values value fill
+ * @param {object} locale the locale to find a translation in.
+ * @param {string} key the key of the translation
+ * @param {object?} values an object containing format values
  */
-function translate(translation, key, values){
-    var message = translation[key] ||= key;
+function translate(locale, key, values){
+    var message = locale[key] ||= key;
     if(values)
     {
         for (const [k, v] of Object.entries(values)) {
@@ -36,8 +60,8 @@ function translate(translation, key, values){
 }
 /**
  * Returns a translator function.
- * @param {string?} prefix prefix to all translation keys
- * @param {string} key prefix to all translation keys
+ * @param {string?} prefix prefix to the translation ket
+ * @param {string} key the translation key used to find a translation
  */
 function combineKey(prefix, key){
     if(prefix && prefix != null)
@@ -51,8 +75,8 @@ function combineKey(prefix, key){
 /**
  * Returns a translator function.
  * @param {string?} prefix prefix to all translation keys
- * @return {(key: string, values: object?) => string} translation function.
+ * @return {(key: string, values: object?) => string} the translation function.
  */
 export function useTranslation(prefix){
-    return useCallback((s, v = {}) => translate(translation, combineKey(prefix,s), v), [translation]);
+    return useCallback((s, v = {}) => translate(currentLocale, combineKey(prefix,s), v), [currentLocale]);
 }
